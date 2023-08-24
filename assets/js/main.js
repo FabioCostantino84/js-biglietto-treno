09179650c02f0e0fcb4b002fede871cfc4ccbca9
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-24 11:28:53 +0200

    add fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,8 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va stampato in forma umana (ovvero con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca nella documentazione di JS.
-*/
 
-/* 
-tool:
+##tool
 -prompt x2
 -number
 -let/const
@@ -17,33 +15,33 @@ tool:
 -console log
 */
 
+
+
 // chiedere all'utente quanti km deve percorrere
 const distance = Number(prompt('type distance in km'));
 
 // chiedere l'età dell'utente
 const userAge = Number(prompt('type yuor age'));
 
+let priceKm = Number(0.21);
+
 let totalPrice;
 
-if (userAge < 18 + 1) {
+if (userAge < 18) {
     // calcolo prezzo minorenni
-    const priceKm = Number(0.21);
-    const underDiscount = Number(0.21 * 20 / 100);
-    const totalPrice = Number(distance * (priceKm - underDiscount));
-    document.getElementById('price').innerHTML = totalPrice.toFixed(2);
-    console.log(totalPrice);
+    underDiscount = Number(0.21 * 20 / 100);
+    totalPrice = Number(distance * (priceKm - underDiscount));
+
 } else if (userAge > 65 - 1) {
     // calcolo prezzo over 65
-    const priceKm = Number(0.21);
-    const overDiscount = Number(0.21 * 40 / 100);
-    const totalPrice = Number(distance * (priceKm - overDiscount));
-    document.getElementById('price').innerHTML = totalPrice.toFixed(2);
+    overDiscount = Number(0.21 * 40 / 100);
+    totalPrice = Number(distance * (priceKm - overDiscount));
     console.log(totalPrice);
 } else {
     // calcolo prezzo intero
-    const priceKm = Number(0.21);
-    const totalPrice = Number(distance * priceKm);
-    document.getElementById('price').innerHTML = totalPrice.toFixed(2);
+    totalPrice = Number(distance * priceKm);
     console.log(totalPrice);
 }
 // stampo il prezzo del biglietto
+document.getElementById('price').innerHTML = totalPrice.toFixed(2);
+
